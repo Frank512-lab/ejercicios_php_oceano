78006ac7f682a28d5ff674825dc02d52a3813410
Frank512-lab <franciscosignes1978@gmail.com>
2025-01-25 19:21:29 +0100

    Cambios realizados en los archivos

--- 02_RecogidaDeDatos/datos.php
+++ 02_RecogidaDeDatos/datos.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recogida de datos</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    
+    <h1>Sumando dos números</h1>
+    <div class="formulario-container">
+        <h2>Formulario de ingreso de números</h2>
+        <form action="suma.php">
+            Primer número: <input type="number" name="a"><br>
+            Segundo número: <input type="number" name="b"><br>
+            <input type="submit" value="Sumar">
+        </form>
+
+    </div>
+
 </body>
 </html>
---
+++ 02_RecogidaDeDatos/suma.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suma</title>
+</head>
+<body>
+    <?php
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    echo "La suma de $a mas $b es: " , $a +$b;//código procedente del GitHub de Luis José Sánchez
+    ?>    
+</body>
+</html>

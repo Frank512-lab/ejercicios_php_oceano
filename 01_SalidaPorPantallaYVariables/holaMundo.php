d6cdefb5350653655f7c628174bec21470b6f874
Frank512-lab <franciscosignes1978@gmail.com>
2025-01-25 17:19:27 +0100

    Primer ejercicio php Océano

---
+++ 01_SalidaPorPantallaYVariables/holaMundo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo con php</title>
+</head>
+<body>
+    <?php
+    echo "Hola Mundo <br>";//primera instrucción en php
+    
+    echo "Probando a hacer un push a GitHub con este código";
+    ?>
+
+</body>
+</html>

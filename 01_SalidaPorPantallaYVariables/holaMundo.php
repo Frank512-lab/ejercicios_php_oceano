cee6b2d74d38936cc699c50d067f438320a233c3
Frank512-lab <franciscosignes1978@gmail.com>
2025-01-25 18:01:23 +0100

    Modificación del nombre de la página

--- 01_SalidaPorPantallaYVariables/holaMundo.php
+++ 01_SalidaPorPantallaYVariables/holaMundo.php
@@ -3,13 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola Mundo con php</title>
+    <title>Aprendiendo PHP y GitHub</title>
 </head>
 <body>
     <?php
     echo "Hola Mundo <br>";//primera instrucción en php
     
-    echo "Probando a hacer un push a GitHub con este código";
+    echo "Probando a hacer un push a GitHub con este código <br>";//commit inicial
+    
+    echo "Quiero hacer una modificación del código con un recommit";
+
     ?>
 
 </body>
